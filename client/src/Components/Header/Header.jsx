ad371b78f84c1e4b4d0e453302d9869b09b68d54
deecarneiro <deysedayane.o.c@gmail.com>
2019-09-08 00:03:08 -0300

    Assets, Favicon, Logo

--- client/src/Components/Header/Header.jsx
+++ client/src/Components/Header/Header.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import styles from "./Header.module.scss";
 import Button from "../Button/Button";
 import { Link } from "react-router-dom";
+import logo from "../../assets/map-icon.png"
 
 const Header = () => {
     return (
         <nav className={`${styles.nav} navbar navbar-expand-lg navbar-light`}>
             <div className={styles.bodyHeader}>
                 <div className={styles.leftHeader}>
-                    <a className="navbar-brand" href="/#"> <img src="C:\Users\Deyse\Desktop\Quaesitum-GIS\client\src\assets\map-icon.png" alt="Quaesitum-GIS" />Quaesitum GIS</a>
+                    <a className="navbar-brand" href="/#"> <img src={logo} style={{ width: '35px' }} alt="Quaesitum-GIS" /> Quaesitum GIS</a>
                     <ul className="navbar-nav mr-auto">
                         <li className={`nav-item active ${styles.option}`}>
                             <a className="nav-link" href="/#">Cadastro</a>
