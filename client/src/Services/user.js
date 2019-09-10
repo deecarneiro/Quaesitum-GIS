d743f30c3f70a4c13d226f48feadbab890d545f8
Joao Victor Oliveira <jvv99.oliveira@gmail.com>
2019-09-10 20:31:52 -0300

    refactor assets

---
+++ client/src/Services/user.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+const API = "https://localhost:5000"; 
+
+const user = {
+    saveUser: (name, email, password) => {
+        const url = API + "/";
+        const data = {
+            name: name,
+            email: email,
+            password: password
+        }
+        return axios.post(url, data);
+    },
+    loadUser: (id) => {
+        const url = API + "/" + id;
+        return axios.get(url);
+    }
+}
+export default user;
