bd0ac70f2a08017b08fadcaf1ad3a664ddc6c97a
João Victor Oliveira <joao.oliveira@pitang.com>
2019-09-13 13:48:24 -0300

    conexao node/react

--- src/app.js
+++ src/app.js
@@ -3,8 +3,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
 const router = express.Router();
 
 //Connecting database
