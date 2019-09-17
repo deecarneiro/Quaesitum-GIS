82da6f044ba487917afc84bb9790a0f0066b27b9
Joao Victor Oliveira <jvv99.oliveira@gmail.com>
2019-09-16 21:20:38 -0300

    show listMaps in home

--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import styles from "./Home.module.scss";
+import MapList from "./MapList/MapList";
+import ConstantList from "./ConstantList";
 
 const Home = () => {
-    return <h1>Home</h1>
+
+    const list = ConstantList;
+    //const list = null;
+
+    const contentMap = (list !== null && list.length !== 0) ? <MapList listMap={list} /> :
+        <p className={styles.message}>Nenhum mapa salvo!</p>;
+
+    return (
+        <>
+            <div className={styles.titleArea}>
+                <h3>Mapas</h3>
+            </div>
+            <div className={styles.contentMaps}>
+                {contentMap}
+            </div>
+        </>
+    )
 }
 
 export default Home;
