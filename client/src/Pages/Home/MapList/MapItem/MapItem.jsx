9711681dbc5bfa19140b6a6479244bfba6063849
Joao Victor Oliveira <jvv99.oliveira@gmail.com>
2019-11-24 11:58:47 -0300

    mapitem with description

--- client/src/Pages/Home/MapList/MapItem/MapItem.jsx
+++ client/src/Pages/Home/MapList/MapItem/MapItem.jsx
@@ -1,16 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./MapItem.module.scss";
 import imageDefault from "../../../../assets/images/ImgRecife.jpg";
 
 const MapItem = (props) => {
-    const { name, img = imageDefault, setMap, index} = props;
+    const { map, setMap, index } = props;
+    const img = map.img ? map.img : imageDefault;
+    const [hoverContent, setHoverContent] = useState(styles.none);
+    console.log(map);
 
     return (
-        <div className={`card ${styles.map}`} onClick={setMap} key={index}>
+        <div className={`card ${styles.map}`} onClick={setMap} key={index} 
+            onMouseEnter={() => setHoverContent(styles.block)} onMouseLeave={() => setHoverContent(styles.none)}>
             <a className={styles.mapBody}>
                 <img src={img} className="card-img-top" alt="_MapImg" />
                 <div className="card-body">
-                    <h5 className="card-title">{name}</h5>
+                    <div className={hoverContent}>
+                        <p>{map.description}</p>
+                    </div>
+                    <h5 className="card-title">{map.name}</h5>
                 </div>
             </a>
         </div>
