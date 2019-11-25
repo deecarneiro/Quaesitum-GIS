7c3647859f43cd2ec69c5e0ed8ae38cd2b961e58
João Victor Oliveira <joao.oliveira@pitang.com>
2019-11-25 10:46:37 -0300

    flip

--- client/src/Pages/Home/MapList/MapItem/MapItem.jsx
+++ client/src/Pages/Home/MapList/MapItem/MapItem.jsx
@@ -1,27 +1,28 @@
-import React, { useState } from "react";
+import React from "react";
 import styles from "./MapItem.module.scss";
 import imageDefault from "../../../../assets/images/ImgRecife.jpg";
 
-const MapItem = (props) => {
-    const { map, setMap, index } = props;
-    const img = map.img ? map.img : imageDefault;
-    const [hoverContent, setHoverContent] = useState(styles.none);
-    console.log(map);
+const MapItem = props => {
+  const { map, setMap, index } = props;
+  const img = map.img ? map.img : imageDefault;
 
-    return (
-        <div className={`card ${styles.map}`} onClick={setMap} key={index} 
-            onMouseEnter={() => setHoverContent(styles.block)} onMouseLeave={() => setHoverContent(styles.none)}>
-            <a className={styles.mapBody}>
-                <img src={img} className="card-img-top" alt="_MapImg" />
-                <div className="card-body">
-                    <div className={hoverContent}>
-                        <p>{map.description}</p>
-                    </div>
-                    <h5 className="card-title">{map.name}</h5>
-                </div>
-            </a>
+  return (
+    <div className={`card ${styles.flipCard}`} onClick={setMap} key={index}>
+      <div className={styles.cardInner}>
+        <div className={styles.cardFront}>
+          <img src={img} className="card-img-top" alt="_MapImg" />
+          <div className="card-body">
+            <h5 className="card-title">{map.name}</h5>
+          </div>
         </div>
-    )
-}
+        <div className={styles.cardBack}>
+          <div className="card-body">
+            <p className={styles.content}>{map.description}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default MapItem;
